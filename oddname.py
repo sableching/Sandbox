9adf5e19e405d5612ba679537e0fc764598d2154
sableching <31094024+sableching@users.noreply.github.com>
2018-03-28 15:59:44 +0800

    Add name printing program

--- oddname.py
+++ oddname.py
@@ -1,1 +1,9 @@
 """Sable Ching"""
+
+while True:
+    name = str(input("Please enter your name: \n"))
+    if name == '':
+        print("Invalid, please enter your name.")
+    else:
+        print(name[1::2])
+        break
